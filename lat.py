0e9e21b4a49d4063e08613b971b6a43345d51c60
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-10-03 22:44:42 +0530

    Add files via upload
    Latitude of a city from the equator

---
+++ lat.py
@@ -0,0 +1,21 @@
+cities = ['New Delhi', 'Paris', 'Rome', 'Brisbane', 'Berlin', 'Buenos Aires', 'London', 'New York']
+citieslat = [28, 48, 41, 27, 52, -34, 51, 40]
+distanceinfo = []
+distfromeq = 0
+print('*****        Welcome to the Latitude to distance from equator converter!********    ')
+
+i=input("Enter the cities: ")
+if i in list(cities):
+    j=cities.index(i)
+    k=citieslat[j]
+    print(i,":",k*69,"miles from equator")
+else:
+    print("It is case sensitive or may not be in our list")
+    exit()
+
+
+
+
+
+    
+  
